750aa8404b8dbc1e6c5ccb5c2987a50e8b9e80ec
Deepak251290 <deepak.pandey529@gmail.com>
2025-11-07 18:34:10 +0530

    maths and numbers

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,44 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+
+
+// console.log(balance.toString());
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(2));
+
+const uniqueNumber = 12.234
+// console.log(uniqueNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// **************************Maths***************************************
+
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(2.9));
+// console.log(Math.round(2.3));
+// console.log(Math.floor(3.6));
+// console.log(Math.min(2,4,5,1));
+
+// console.log(Math.random());
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 30
+const max = 40
+
+console.log(Math.floor((Math.random()* (max - min + 1)) + min));
+
+
+
+
+
+
+
+
+
+
