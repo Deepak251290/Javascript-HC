4ae5c71a862103c5803dd7df28ec77914f119fc7
Deepak251290 <deepak.pandey529@gmail.com>
2025-11-07 12:19:32 +0530

    Memory management Stack & Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,3 +12,25 @@
 // 1. Object
 //    a. Array 
 //    b. Function
+
+// *****************************************************************************
+
+// Stack (Primitive Data Types) vs Heap (Non-Primitive Data Types)
+
+let myYoubeChannel = "Deepak Pandey"
+let anotherChannel = myYoubeChannel
+anotherChannel = "Tech Deepak"
+
+// console.log(myYoubeChannel)
+// console.log(anotherChannel);
+
+let myChannel ={
+    channelName: "Tech Deepak"
+}
+
+let myChannel1 = myChannel
+myChannel1.channelName = "Deepak IT"
+
+console.log(myChannel.channelName); 
+console.log(myChannel1.channelName); 
+
