c1e7cbe5bc7b3d3e973a0537dfebfc385b6b5fd0
Deepak251290 <deepak.pandey529@gmail.com>
2025-11-07 19:16:43 +0530

    Dates in JS

---
+++ 01_basics/07_DatesinJS.js
@@ -0,0 +1,13 @@
+// console.log(Date());
+// console.log(typeof Date);
+
+
+// let todayDate = new Date(2025, 10, 7)
+let todayDate = new Date(2025, 10, 7, 7, 0)
+// console.log(todayDate.toDateString());
+console.log(todayDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+
